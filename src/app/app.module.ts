ff363203a40659ec8b75d50c43caf193e15acfbf
Piter265 <p.anuszkiewicz527@gmail.com>
2023-08-30 17:09:30 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './presentation/pages/login/login.component';
+import { MainComponent } from './presentation/pages/main/main.component';
+import { ErrorComponent } from './presentation/pages/error/error.component';
+import { LoginFormComponent } from './presentation/pages/login/login-form/login-form.component';
+import { RegisterFormComponent } from './presentation/pages/login/register-form/register-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    ErrorComponent,
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
